cddc7c340e82a94932ca3a5d1e5cc3aef6755dc9
TayingThao <tayingthao18@gmail.com>
2025-12-05 06:23:30 -0800

    client-side rendering

--- packages/proto/src/inventory-view.ts
+++ packages/proto/src/inventory-view.ts
@@ -1,22 +1,65 @@
 import { html, css, LitElement } from "lit";
+import { property, state } from "lit/decorators.js";
+import "./inventory-card.js";
+
+interface InventoryItem {
+    name: string;
+    qty: number;
+    imgSrc: string;
+}
 
 export class InventoryViewElement extends LitElement {
-  override render() {
-    return html`
-      <div class="inventory-grid">
-        <slot></slot>
-      </div>
-    `;
-  }
+@property({ type: String })
+src?: string;
 
-  static styles = css`
-    .inventory-grid {
-      display: grid;
-      grid-template-columns: repeat(3, 1fr);
-      gap: 10px;
-      padding: 10px;
+@state()
+items: InventoryItem[] = [];
+
+connectedCallback() {
+    super.connectedCallback();
+    if (this.src) {
+      this.hydrate(this.src);
     }
-  `;
+}
+
+hydrate(src: string) {
+    fetch(src)
+    .then(res => res.json())
+    .then((json: any) => {
+        if(json)    {
+            this.items = json.map((obj: any) => ({
+                name: obj.name,
+                qty: obj.qty,
+                imgSrc: obj.imgSrc
+            }));
+        }
+    })
+}
+
+renderItem(item: InventoryItem) {
+    return html`
+      <inventory-card img-src=${item.imgSrc} qty=${item.qty}>
+        ${item.name}
+      </inventory-card>
+    `;
+}
+
+override render() {
+    return html`
+        <div class="inventory-grid">
+            ${this.items.map(this.renderItem)}
+        </div> 
+    `;
+}
+
+static styles = css`
+    .inventory-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+        gap: 10px;
+    padding: 10px;
+    }
+`;
 }
 
 customElements.define("inventory-view", InventoryViewElement);
